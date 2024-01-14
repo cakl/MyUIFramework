e96e28930f9737fd1ed42db38e38214d7db18d6d
sebo <sebastian.bock@bluewin.ch>
2024-01-14 18:00:27 +0100

    Initial Commit

---
+++ MyUIFramework/MyUIFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyUIFramework.h
+//  MyUIFramework
+//
+//  Created by sbo on 14.01.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyUIFramework.
+FOUNDATION_EXPORT double MyUIFrameworkVersionNumber;
+
+//! Project version string for MyUIFramework.
+FOUNDATION_EXPORT const unsigned char MyUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyUIFramework/PublicHeader.h>
+
+
